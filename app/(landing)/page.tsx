697a5ab42bd69bf2abd30b5b2d4e7ce794cfd9dd
Redve <redvelolbanned@gmail.com>
2024-03-01 14:47:07 +0100

    Initial landing page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,38 @@
+import { Medal } from "lucide-react";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import localFont from "next/font/local"
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const marketingPage = () => {
+    return(
+        <div className="flex items-center justify-center flex-col">
+            <div className={cn("flex items-center justify-center flex-col")}>
+                <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+                    <Medal className="h-6 w-6 mr-2"/>
+                    ME2073
+                </div>
+                <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6"> 
+                        Made by group name: TBA!
+                </h1>
+                <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-700 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+                    Made By Students For Students
+                </div>
+            </div>
+            <div className={cn("text-sm md-text-xl text-neautral-200 mt-4 max-w-xs md:max-w-2xl text-center mx-auto")}>
+                Course Evaluation System specifaclly designed for students to provide feedback on their courses and professors.
+                This platform is mainly intended for KTH courses and professors, but can be used by any university.
+            </div>
+            <Button className="mt-6 size=lg" asChild>
+                <Link href="/">
+                    Learn more about the project and the team
+                </Link>
+            </Button>
+        </div>
+    );
+};
+
+export default marketingPage;
+
